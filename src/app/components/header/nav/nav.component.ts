a28a20c3a068e6a6b8613e2dbaae419a7a2dd801
Kaspar Vollenweider <vok@panter.ch>
2025-06-24 13:04:35 +0200

    feat(header-nav): add main nav options scafold

--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
+  standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
+    RouterLink,
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
